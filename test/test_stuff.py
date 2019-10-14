9acc9b70ec23d2629eebe85488f8888a580acdc5
f160264 <54073770+f160264@users.noreply.github.com>
2019-10-14 13:38:25 +0200

    Update test_stuff.py

--- test/test_stuff.py
+++ test/test_stuff.py
@@ -4,7 +4,7 @@ from .context import stuff
 
 class Stuff(TestCase):
     def test_two_plus_two(self):
-        self.assertEquals(8, stuff.two_plus_two(), "what was two plus two again?")
+        self.assertEquals(4, stuff.two_plus_two(), "what was two plus two again?")
 
     def test_four_plus_four(self):
         self.assertEquals(8, stuff.four_plus_four(), "what was four plus four again?")
